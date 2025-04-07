9e51f08bdeffae99ee1d4a7c526d9528d797e8a4
Yomiblack <yomiblacck@gmail.com>
2025-04-07 21:04:10 +0100

    environmental variable modified

--- frontend/src/app/components/home.component.ts
+++ frontend/src/app/components/home.component.ts
@@ -41,7 +41,7 @@ export class HomeComponent {
 
     // Send user message to the server
     try {
-      const response = await fetch('http://localhost:3000/api/chat', {
+      const response = await fetch('https://sbsc-delta.vercel.app/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
